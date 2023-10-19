04db4f27ab967cdc29e9470a0037081cbdebbb43
Teresefal <56402723+Teresefal@users.noreply.github.com>
2023-10-19 14:50:20 +0000

    added second timer

--- app-swap.js
+++ app-swap.js
@@ -22,3 +22,18 @@ function startTimer() {
         }
     }, 1000);
 }
+
+function startTimer2() {
+    var time = 25 * 60;
+    clearInterval(interval);
+    interval = setInterval(function() {
+        time--;
+        var minutes = Math.floor(time / 60);
+        var seconds = time % 60;
+        document.getElementById('timer').textContent = 'Таймер: ' + minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+        if (time <= 0) {
+            alert('Следай перерыв!');
+            time = 25 * 60;
+        }
+    }, 1000);
+}
